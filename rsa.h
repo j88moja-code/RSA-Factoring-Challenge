08bcb97916a820626a0a707e7135c76790980318
j88moja-code <sitholent+github@gmail.com>
2022-09-16 04:42:31 -0700

    RSA Factoring Challenge

---
+++ rsa.h
@@ -0,0 +1,9 @@
+#ifndef _RSA_H_
+#define _RSA_H_
+
+void printtab(unsigned long long int tab[], unsigned long long int t);
+void divnumber(int tab[], unsigned long long int n);
+void copytab(int tab[]);
+
+
+#endif
